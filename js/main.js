6b2acf4cd0113426bfd0de3d5fe77bfe6b87552f
Rea <dilemajire@gmail.com>
2020-09-21 16:26:14 -0500

    En este primer commit se agregan todos los archivos para comenzar la edición

---
+++ js/main.js
@@ -0,0 +1,13 @@
+eventListeners();
+
+function eventListeners() {
+  if(document.getElementById('info_contacto')){
+    document.getElementById('info_contacto').addEventListener('submit', validate_form_contact);
+  }
+}
+
+function validate_form_contact(e){
+  e.preventDefault();
+
+  alert("Validamos");
+}
